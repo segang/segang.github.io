8a214cdf7f9a0daf317f37dca0f63fcfff9fe5f2
segang <73174829+segang@users.noreply.github.com>
2020-12-23 04:08:15 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $('.segangBanner>ul>li').hide();
-$('.segangBanner>ul>li:first-child').show(); setInterval(function(){
+$('.segangBanner>ul>li:first-child').show();
+setInterval(function(){
   $('.segangBanner>ul>li:first-child').fadeOut().next().fadeIn().end(6000).appendTo('.segangBanner>ul');
 },3000);
